d74813f39b075f2b5fd9a54233eeed075c52442e
Nihat <abdullazadenihat@gmail.com>
2023-05-05 11:19:44 +0400

    prototypes added

--- 8-prototypes-closures/main.js
+++ 8-prototypes-closures/main.js
@@ -1,50 +1,50 @@
-const animal = {
-  walk : function () { console.log("walking ...") },
-
+String.prototype.capitalize = function () {
+  return this.slice(0, 1).toLocaleUpperCase() + this.slice(1).toLocaleLowerCase()
 }
-
-String.prototype.capitalize = function (){
-  return this.slice(0,1).toLocaleUpperCase() + this.slice(1).toLocaleLowerCase()
+String.prototype.insertPlus = function () {
+  return this.slice(0, Math.floor(this.length / 2)) + "+" + this.slice(Math.floor(this.length / 2),)
 }
+Number.prototype.double = function () { return this * 2 }
 
-
-String.prototype.insertPlus = function (){
-  return this.slice(0,Math.floor(this.length/2)) + "+" + this.slice(Math.floor(this.length/2),)
-}
-
-Number.prototype.double = function () { return this*2 }
-
-// console.log("debuggin".insertPlus())
-
-
-// console.log( 15..dou//9ble() )
-console.log( (15).double() )
-
-
-Array.prototype.push = function(x){
+Array.prototype.push = function (x) {
   console.log('de')
-  return [...this,x]
+  return [...this, x]
 }
 
-console.log([1,5,10].push(30))
-
-
-function add (x){
-  let total=0
-  return function(){
+function add(x) {
+  let total = 0
+  return function () {
     total++
     return total
   }
 }
 
-function rectangle()
+function Rectangle(w, h) {
+  this.w = w
+  this.h = h
+}
+Rectangle.prototype.getArea = function () {
+  return this.w * this.h
+}
 
 
-let a = add()
+function Person(name, age) {
+  this.name = name
+  this.age = age
+}
+Person.prototype.introduce = function () {
+  return "Hi " + this.name + " " + this.age
+}
 
-a()
-a()
-a()
 
-console.log(a())
+function Animal(name) {
+  this.name = name
+}
+Animal.prototype.eat = function () {
+  console.log("Animal is eating...")
+}
 
+
+Array.prototype.last = function () {
+  return this.length == 0 ? -1 : this.at(-1)
+}
